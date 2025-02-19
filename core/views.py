f4092277dcf4ad8715591ce3664f49ab7a5e88e9
VladimirMonin <vladimir.monin2016@yandex.ru>
2025-02-19 22:14:01 +0500

    lesson_70: ListView для заявок - доступ только для персонала

--- core/views.py
+++ core/views.py
@@ -47,3 +47,48 @@ class IndexView(CreateView):
         context['masters'] = Master.objects.all()
         context['services'] = Service.objects.all()
         return context
+    
+
+from django.views.generic import ListView
+from django.shortcuts import redirect
+from django.db.models import Q
+from .models import Visit, Master
+
+
+class VisitListView(ListView):
+    model = Visit
+    template_name = 'visit_list.html'
+    context_object_name = 'visits'
+    paginate_by = 1
+
+    def get_queryset(self):
+        """Формируем QuerySet с учетом поиска и фильтрации по мастеру"""
+        queryset = Visit.objects.all().order_by('-created_at')
+
+        # Получаем параметры из GET-запроса
+        search_query = self.request.GET.get('q', '')
+        master_id = self.request.GET.get('master', '')
+
+        # Фильтрация по поисковому запросу (имя или телефон)
+        if search_query:
+            queryset = queryset.filter(Q(name__icontains=search_query) | Q(phone__icontains=search_query))
+
+        # Фильтрация по мастеру
+        if master_id:
+            queryset = queryset.filter(master_id=master_id)
+
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        """Добавляем в контекст список мастеров и текущие фильтры"""
+        context = super().get_context_data(**kwargs)
+        context['masters'] = Master.objects.all()
+        context['search_query'] = self.request.GET.get('q', '')
+        context['selected_master'] = self.request.GET.get('master', '')
+        return context
+
+    def dispatch(self, request, *args, **kwargs):
+        """Запрещаем доступ не-администраторам"""
+        if not request.user.is_staff:
+            return redirect('main_page')  # Перенаправляем на главную
+        return super().dispatch(request, *args, **kwargs)
