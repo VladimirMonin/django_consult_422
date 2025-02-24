207e97c778ad6d98f03837fa8400da99d422478f
VladimirMonin <vladimir.monin2016@yandex.ru>
2025-02-24 23:27:51 +0500

    feat: добавил обработчик сигналов для автоматической проверки и изменения статуса отзывов

--- core/utlils.py
+++ core/utlils.py
@@ -1,29 +1,30 @@
 import os
 from mistralai import Mistral
-# from django.conf import settings  # Правильный способ импорта настроек Django
-from prompts import MISTRAL_REVIEW_PROMPT
-from dotenv import load_dotenv
-import os
+from django.conf import settings
+from .prompts import MISTRAL_REVIEW_PROMPT
 
-load_dotenv()
+MISTRAL_API_KEY = settings.MISTRAL_API_KEY
+MISTRAL_MODEL = settings.MISTRAL_MODEL
 
-MISTRAL_API_KEY = os.getenv('MISTRAL_API_KEY')
-MISTRAL_MODEL = os.getenv('MISTRAL_MODEL')
-
-REVIEW = "Всё классно! Но кажется от Бородоча немного несло спиртом. Хотя ему можно простить, он же Бро. В целом рекомендую, если вас это не смущает"
-
-PROMT = MISTRAL_REVIEW_PROMPT.format(review_text=REVIEW)
-
-# Инициализация клиента
-client = Mistral(api_key=MISTRAL_API_KEY)
-
-chat_response = client.chat.complete(
-    model=MISTRAL_MODEL,
-    messages=[
-        {
-            "role": "user", 
-            "content": PROMT
-        }
-    ]
-)
-print(chat_response.choices[0].message.content)
+def check_review(review_text):
+    # Инициализация клиента
+    client = Mistral(api_key=MISTRAL_API_KEY)
+    prompt = MISTRAL_REVIEW_PROMPT.format(review_text=review_text)
+    chat_response = client.chat.complete(
+        model=MISTRAL_MODEL,
+        messages=[
+            {
+                "role": "user",
+                "content": prompt
+            }
+        ]
+    )
+    response_text = chat_response.choices[0].message.content.lower()
+    # Вывод в консоль ответа
+    print(response_text)
+    if "true" in response_text.lower():
+        return True
+    elif "false" in response_text.lower():
+        return False
+    else:
+        return False
