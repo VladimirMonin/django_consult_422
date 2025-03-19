abd1dde9393c6bee76ab773fb4cadc05fe0cc62d
VladimirMonin <vladimir.monin2016@yandex.ru>
2025-03-19 22:11:29 +0500

    feat: обновлен процесс деплоя, добавлена сборка статических файлов и перезапуск сервиса gunicorn. Исправлено значение DEBUG на False для повышения безопасности в продакшене. Обновлены пути для статических и медиа файлов.

--- barber/settings.py
+++ barber/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "127.0.0.1",
@@ -137,17 +137,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = "static/"
+
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
-
-
-MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "media"
+STATIC_URL = '/static/'
+STATIC_ROOT = '/home/v/django_consalt_422/static'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = '/home/v/django_consalt_422/media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
